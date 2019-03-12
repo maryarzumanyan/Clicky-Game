96416eb6ae43f9704b625e846f25945eefb76998
Mary Arzumanyan <mary.arzumanyan@gmail.com>
2019-03-11 23:25:17 -0700

    Updates

---
+++ precache-manifest.db00768d86526256899a82207cbc6250.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "86b96b82676d5c564e4c",
+    "url": "/Clicky-Game/static/css/main.f9b0f1da.chunk.css"
+  },
+  {
+    "revision": "86b96b82676d5c564e4c",
+    "url": "/Clicky-Game/static/js/main.1224ec1a.chunk.js"
+  },
+  {
+    "revision": "61a0df336c60dbe48a52",
+    "url": "/Clicky-Game/static/js/runtime~main.43da1946.js"
+  },
+  {
+    "revision": "ac164eeb7fe39897100f",
+    "url": "/Clicky-Game/static/js/2.aa7a0768.chunk.js"
+  },
+  {
+    "revision": "396c8f0a932042fde2c9eb82f27a4f63",
+    "url": "/Clicky-Game/index.html"
+  }
+];
